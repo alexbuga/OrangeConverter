a08d7ff6f2109b41703e5021a17befe8925f787a
Alex Buga <alex.buga@gmail.com>
2020-03-19 09:23:00 +0200

    Removed pods

--- Pods/Target Support Files/Pods-OrangeConverter/Pods-OrangeConverter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OrangeConverter : NSObject
-@end
-@implementation PodsDummy_Pods_OrangeConverter
-@end
--- Pods/Target Support Files/Pods-OrangeConverterTests/Pods-OrangeConverterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OrangeConverterTests : NSObject
-@end
-@implementation PodsDummy_Pods_OrangeConverterTests
-@end
